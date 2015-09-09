9a24c25d2e3e48f21e1947a882adbd289b11e0fe
littcai <littcai@gmail.com>
2015-09-09 15:45:04 +0800

    [^] 升级到logback

--- littcore-security/src/test/java/com/littcore/security/SecurityTest.java
+++ littcore-security/src/test/java/com/littcore/security/SecurityTest.java
@@ -33,8 +33,8 @@ public class SecurityTest {
 	@Before
 	public void init() throws Exception
 	{
-		RSATool tool = SecurityFactory.genRSA("shgc");
-		tool.store("C:\\priKey.key", "C:\\pubKey.key");
+		RSATool tool = SecurityFactory.genRSA("jfqmt");
+		tool.store("E:\\jfqmt-pri.key", "E:\\jfqmt-pub.key");
 	}
 	
 	@Test
