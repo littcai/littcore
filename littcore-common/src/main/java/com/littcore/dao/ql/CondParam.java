8f0eb8ba467f54d393e7d863423e38d73bf3b947
littcai <littcai@gmail.com>
2016-09-05 10:20:46 +0800

    [^] 排序字段要支持括号

--- littcore-common/src/main/java/com/littcore/dao/ql/CondParam.java
+++ littcore-common/src/main/java/com/littcore/dao/ql/CondParam.java
@@ -154,12 +154,12 @@ public class CondParam
         throw new IllegalArgumentException("Illegal sort order");
       }
       
-      if(sortField.length()>20)
+      if(sortField.length()>30)
       {
         throw new IllegalArgumentException("Illegal sort field");
       }
       
-      if(!RegexUtils.validateSoft(sortField, "^[A-Za-z0-9_\\.]+$"))
+      if(!RegexUtils.validateSoft(sortField, "^[A-Za-z0-9_\\.\\(\\)]+$"))
       {
         throw new IllegalArgumentException("Illegal sort parameter");
       }
